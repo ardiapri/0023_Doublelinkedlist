017e3e13c1f8c4d6b22a1fee2f7b4f3cc949e974
ardiapri <ardiapri882@gmail.com>
2024-05-04 11:58:09 +0700

    menambahkan insert

--- doulelinkedlist.cpp
+++ doulelinkedlist.cpp
@@ -8,6 +8,7 @@ struct Node
     Node *next;
     Node *prev;
 };
+
 Node *START = NULL;
 
 void addNode()
@@ -17,4 +18,25 @@ void addNode()
     cin >> newNode->noMhs;
     cout << "\nEnter the name of the student: ";
     cin >> newNode->name;
-}
+
+// insert the new node int the list
+    if (START == NULL || newNode->noMhs <= START->noMhs)
+    { // insert the new node at the beginning. 2
+
+        if (START != NULL && newNode->noMhs == START->noMhs)
+        {
+            cout << "\033[31mDuplicate roll numbers not allowed\033]0m" << endl;
+            return;
+        }
+        // if the list is empty, make the new node the START
+        newNode-> next = START; // 3. make the new node point to the first node
+        if (START != NULL)
+        {
+            START->prev = newNode; // 4. make the first node point to the new node
+        }
+        newNode->prev = NULL; // 5. make the new node point to NULL
+        START = newNode;      // 6. make the new node the first node
+    }
+
+
+
